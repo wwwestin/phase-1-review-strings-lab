b0dc37dfa9746c0ac689bd0db21d377e086451d9
Westin Humble <westin.h@gmail.com>
2022-01-11 08:52:41 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!"
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!"
+let shortGreeting = "Welcome, " + currentUser[0] + "!"
